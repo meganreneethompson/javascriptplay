16363613948788fc73726ff32c7b948838725e48
Meg Thompson <128401836+meganreneethompson@users.noreply.github.com>
2023-12-11 11:07:36 -0500

    primitive types

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-let name = 'Mosh';
-console.log(name);
+let name = 'Mosh'; //String Literal
+let age = 30; //Number Literal
+let isApproved = true; //Boolean Literal
+let firstName = undefined;
+let lastName = null;
