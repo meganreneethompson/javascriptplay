78bbef6215012a7ed0894ebc2401a7ca694be813
Meg Thompson <128401836+meganreneethompson@users.noreply.github.com>
2023-12-13 12:44:40 -0500

    Logical operator

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// Ternary Operator
+// Logial and (&&)
+//Returns true on console if both operands are true. 
+// Ex: Applicant has high income and good credit score to get mortgage loan 
+//Logicl OR (||) returns true if one operand is true
 
-let points = 110;
-let type = points > 100 ? 'gold' : 'silver';
+let highIncome = true;
+let goodCreditScore = true;
+let eligibleFortLoan = highIncome && goodCreditScore;
 
-console.log(type);
+//Not (!)
+let applicationRefused = !eligibleFortLoan;
+
+console.log(eligibleFortLoan);
