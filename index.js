5135d2df95c576c5b805601acfda9f7fc380f802
Meg Thompson <128401836+meganreneethompson@users.noreply.github.com>
2023-12-13 11:50:05 -0500

    Strict vs Lose Equality

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let x = 1;
+// Strict Equality (Same type + value). Most used. 
 
+console.log (1 === 1);
+console.log ('1' === 1);
 
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x < 1);
-console.log(x <= 1);
- 
-console.log(x === 1);
+// Lose Equality. If it doesn't match it will convert left to match the right
+
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
